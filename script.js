a7be2ebb496f9b6c0d81e96b95941d521493f81a
Jose Salazar <jsalazar@pneumarespiratory.com>
2023-11-16 02:14:53 -0500

    Drew the ball and filled it in, it spawns in the center between the two players

--- script.js
+++ script.js
@@ -9,5 +9,10 @@ function draw(){
     //drawing the rectangle that will be played on
     context.fillStyle = 'orange'
     context.fillRect(0,0,1000,600)
+
+    //drawing the  ball
+    context.fillStyle = 'white'
+    context.arc(500,300,10, 0,Math.PI*2,false) // creates the ball 
+    context.fill() //fills the ball in
 }
 draw()
