6807ae39421694d4e82c96e8ada85f622cf5579d
Jose Salazar <jsalazar@pneumarespiratory.com>
2023-12-14 19:41:53 -0500

    Improved comments to make the script more readable.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('#pongCanvas')
-const context = canvas.getContext('2d')
+const context = canvas.getContext('2d') //Select the canvas element and get its 2D rendering context for drawing
 
 canvas.width = 1000
 canvas.height = 600
@@ -17,13 +17,14 @@ let ballSize = 10
 paddleWidth = 20
 rightScore = 0
 leftScore = 0
+
 // Drawing a ball
-function drawBall(){
+function drawBall(){ 
     context.fillStyle = 'white'
-    context.beginPath();
-    context.arc(ballX,ballY,ballSize, 0,Math.PI*2,false) // creates the ball 
-    context.closePath(); // I am not sure why I need this method
-    context.fill() //fills the ball in
+    context.beginPath(); //Begin a new path to draw the ball
+    context.arc(ballX,ballY,ballSize, 0,Math.PI*2,false)
+    context.closePath(); //Close the path to ensure the shape is complete before filling
+    context.fill() //Fills the ball in
 }
 
 function drawBall2(){
@@ -33,7 +34,7 @@ function drawBall2(){
     context.closePath(); // I am not sure why I need this method
     context.fill() //fills the ball in
 }
-//Drawing Game field
+//Sets up the game field by drawing a rectangle of a specific color
 function drawGamefield(){
     context.fillStyle = 'orange'
     context.fillRect(0,0,1000,600)
@@ -52,7 +53,7 @@ function drawLeftPaddle(){
 }
 
 
-//Resetting the ball
+//Resets the ball by repositioning it to the center and increasing the speed
 function ballReset(){
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
@@ -62,7 +63,7 @@ function ballReset(){
     ballSpeedY += 0.5;
 }
 
-//Will be used to create the points
+//This function will create the scores by rendering text on the canvas
 function makeText(text,x,y){
     context.fillStyle = 'white'
     context.font = '46px Arial'
@@ -87,8 +88,11 @@ function draw(){
     //Drawing left hand side score
     makeText(leftScore,canvas.width/4,50)
 }
+
 let breakbool =false 
+
 function movement(){
+    // Handles the win condition, if a player scores over 6 points then the game alerts the plays
     if(rightScore >= 6){
         alert('Game over, right hand side wins! Click ok to start a new game.')
         breakbool = true
@@ -99,50 +103,52 @@ function movement(){
         breakbool = true
         return alert
     }
+    //Updates the balls position by adding the velocity to its current position
     ballX += ballSpeedX
     ballY += ballSpeedY
     ballX2 += -ballSpeedX
     ballY2 += -ballSpeedY
 
-    //Ball collison with top and bottom
+    //Handles the ball collision with top and bottom
     if (ballY + ballSize > canvas.height || ballY - ballSize< 0){
-        ballSpeedY = -ballSpeedY
+        ballSpeedY = -ballSpeedY //Changes the direction of the ball after collision
     }
-
+    //Handles the ball collision with paddles
     if (ballX < paddleWidth && ballY > leftPaddleY && ballY < leftPaddleY + leftPaddleHeight ||
         ballX > canvas.width - paddleWidth - ballSize && ballY > rightPaddleY && ballY < rightPaddleY + rightPaddleHeight) {
-        ballSpeedX = -ballSpeedX;
+        ballSpeedX = -ballSpeedX;//Changes the direction of the ball after collision
     }
 
-    //Scores
+    //Increases scores and resets the ball to center 
     if(ballX<0){
-        rightScore++
+        rightScore++ //Updates the score
         rightPaddleHeight -=  7
         ballReset()
     }
 
     else if(ballX> canvas.width){
-        leftScore++
+        leftScore++ //Updates the score
         leftPaddleHeight -= 7
         ballReset()
     }
 
 }
-//moving the paddles
+
+//moving the paddles up and down with the arrow keys
 document.addEventListener('keydown', function(event){
     if(event.key === 'ArrowUp'){
-        rightPaddleY = Math.max(rightPaddleY - 15,0)
+        rightPaddleY = Math.max(rightPaddleY - 15,0) //Moves the right paddle up
     }
     else if (event.key==='ArrowDown'){
-        rightPaddleY = Math.min(rightPaddleY +15, canvas.height - rightPaddleHeight)
+        rightPaddleY = Math.min(rightPaddleY +15, canvas.height - rightPaddleHeight) //Moves the right paddle down
         }
 
     else if(event.key === 'w'){
-        leftPaddleY = Math.max(leftPaddleY - 15,0)
+        leftPaddleY = Math.max(leftPaddleY - 15,0) //Moves the left paddle up
     }
 
     else if (event.key ==='s'){
-        leftPaddleY = Math.min(leftPaddleY +15,canvas.height-leftPaddleHeight)
+        leftPaddleY = Math.min(leftPaddleY +15,canvas.height-leftPaddleHeight)//Moves the left paddle down
     }
 })
 
